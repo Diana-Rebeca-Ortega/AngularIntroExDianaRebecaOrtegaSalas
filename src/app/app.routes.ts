b55b37c211f44a720a44fb422a85f7b67b6ce7ef
Diana Rebeca Ortega <diana.os@tecjerez>
2026-03-13 19:08:57 -0600

    Se agrego el menu principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../componentes/home/home';
 import { LoginComponent } from '../componentes/login/login'; 
+import { MenuPrincipalComponent } from '../componentes/menu-principal/menu-principal';
+
+
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-   { path: '', component: HomeComponent }
+  { path: 'menu', component: MenuPrincipalComponent },
+ { path: '', component: HomeComponent }
 ];
